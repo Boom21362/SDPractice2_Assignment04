93a37410866a4ebb641bd120fba45a74d705aa1e
Napat Putsorn <napatput31@hotmail.com>
2026-03-11 14:53:55 +0700

    Change logo & booking margin

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -5,7 +5,7 @@ import TopMenuItem from './TopMenuItem';
 
 export default function TopMenu(){
     return(
-        <div className="h-[50px] bg-white fixed top-0 left-0 right-0 z-30 border-t border-b border-gray-300 flex flex-row-reverse">
+        <div className="h-[50px] bg-white fixed top-0 left-0 right-0 z-30 border-t border-b border-gray-300 flex flex-row-reverse items-center px-5 ">
             <Image src={'/img/logo.png'} className="h-[100%] w-auto"
             alt = 'logo'
             width={0} height={0} sizes='100vh'/>
